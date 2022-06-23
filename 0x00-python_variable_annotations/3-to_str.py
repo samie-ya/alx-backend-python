d3aabe97784d21fdb7e53877b6b8c6155209199d
Samie <samrasolomon40@gmail.com>
2022-06-23 09:32:47 +0000

    Added a file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script will chnage a float to a string"""
+
+
+def to_str(n: float) -> str:
+    """This function will return the string of a float"""
+    return str(n)
