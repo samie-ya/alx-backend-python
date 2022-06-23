354985a35e0a3e29aa3927e101bd8dbabf112af2
Samie <samrasolomon40@gmail.com>
2022-06-23 09:31:34 +0000

    Added a file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script will concatnate string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function will concatenate strings"""
+    return str1 + str2
