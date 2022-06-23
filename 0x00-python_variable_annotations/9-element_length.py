c3e207902b4a103fee97586cd46105fbe90ebe63
Samie <samrasolomon40@gmail.com>
2022-06-23 14:31:42 +0000

    Removed unmatched parenthesis

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,6 +4,6 @@
 from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]):
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """This function will annotate parameters and return values"""
     return [(i, len(i)) for i in lst]
