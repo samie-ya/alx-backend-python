abb43aca1e9f36802d34df1970a4ed8ce908d787
Samie <samrasolomon40@gmail.com>
2022-06-23 09:52:04 +0000

    Removed add ons

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python3
-"""This script will do duck typing"""
-
 from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """This function will annotate parameters and return values"""
     return [(i, len(i)) for i in lst]
