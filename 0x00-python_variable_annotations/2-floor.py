6127f31586f3817284744b46f1d71502334590b2
Samie <samrasolomon40@gmail.com>
2022-06-23 09:32:12 +0000

    Added a file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This script will get the integer of a float"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """This function will return the floor of float"""
+    return math.floor(n)
