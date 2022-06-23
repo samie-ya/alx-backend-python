da30adbf3b34c928fcde6aa6cbb356dd8f9556b2
Samie <samrasolomon40@gmail.com>
2022-06-23 09:46:15 +0000

    Added a file

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This script will do duck typing"""
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """This function will annotate parameters and return values"""
+    return [(i, len(i)) for i in lst]
