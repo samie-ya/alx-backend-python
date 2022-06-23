8d4bb02cfc136b371ef59961acebdaca918d0a25
Samie <samrasolomon40@gmail.com>
2022-06-23 09:29:10 +0000

    Added a file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script will add annontation to function argument"""
+
+
+def add(a: float, b: float) -> float:
+    """This function will add float to return"""
+    return a + b
