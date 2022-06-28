391558a892ce322e4e6f25ce3ac875b5b08d9f4f
Samie <samrasolomon40@gmail.com>
2022-06-28 10:09:03 +0000

    Modified return typehint

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,8 +6,8 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> AsyncGenerator[float, None]:
     """This function will generate random numbers"""
     for i in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
