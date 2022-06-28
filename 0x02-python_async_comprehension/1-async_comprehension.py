a556f60eb2f0563283b184bccf17a1fe27cb409f
Samie <samrasolomon40@gmail.com>
2022-06-28 09:25:16 +0000

    Added one line async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """This function will return random numbers"""
-    newlist = []
-    async for i in async_generator():
-        newlist.append(i)
-    return newlist
+    return [i async for i in async_generator()]
