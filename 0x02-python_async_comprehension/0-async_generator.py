50cffaa9fdb62dfc709f9b16233fe62aacc6e723
Samie <samrasolomon40@gmail.com>
2022-06-28 07:59:46 +0000

    Added files

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This script will create a generator"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator:
+    """This function will generate random numbers"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
