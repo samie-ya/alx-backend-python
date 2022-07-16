899004538e28737193e798559a6c9db14316c2f3
Samie <samrasolomon40@gmail.com>
2022-07-16 18:27:06 +0000

    Added raise test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,15 @@ class TestAccessNestedMap(TestCase):
         """This function will test the given parameterized iterable"""
         self.assertEqual(access_nested_map(nested, path), res)
 
+    @parameterized.expand([
+        ("first", {}, ("a",)),
+        ("second", {"a", 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(self, number: str, nested: Mapping,
+                                         path: Sequence):
+        """This function will test raising errors"""
+        self.assertRaises(KeyError, access_nested_map, nested, path)
+
 
 if __name__ == "__main__":
     unittest.main()
